9819d2aafd9e5f71bed94d0960cb158a937ff148
Miguel Fernandez <miguel.fernandez@docplanner.com>
2025-12-15 13:33:00 +0100

    put top

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full min-h-screen flex flex-col items-center justify-start p-4 pt-8 bg-gray-50 dark:bg-gray-900" style="align-items: center; justify-content: flex-start;">
+  <div class="w-full min-h-screen flex flex-col items-center justify-start pt-8 px-4 bg-gray-50 dark:bg-gray-900">
     <Calculator />
   </div>
 </template>
